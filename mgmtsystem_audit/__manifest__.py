2f0c814fe4d240c1f62512cdb416ac28fb14029d
banqu <banqu@163.com>
2018-11-23 21:45:55 +0800

    up

--- mgmtsystem_audit/__manifest__.py
+++ mgmtsystem_audit/__manifest__.py
@@ -2,8 +2,8 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Management System - Audit",
-    "version": "11.0.1.0.0",
-    "author": "Savoir-faire Linux, Odoo Community Association (OCA)",
+    "version": "12.0.1.0.0",
+    "author": "OnGood, Savoir-faire Linux, Odoo Community Association (OCA)",
     "website": "http://www.savoirfairelinux.com",
     "license": "AGPL-3",
     "category": "Management System",
